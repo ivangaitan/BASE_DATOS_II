5b3cb91ec3c0a78e4e7f6cd833d929d0642ac0bf
ivangaitan <ivangaitan.kp@gmail.com>
2019-12-11 16:12:34 -0300

    Actualizacion clasd 4

--- clase4.sql
+++ clase4.sql
@@ -10,7 +10,7 @@ SELECT title, rating, special_features FROM film WHERE special_features LIKE '%B
 5-
 SELECT first_name, last_name from film, film_actor, actor WHERE film.film_id = film_actor.film_id and film_actor.actor_id = actor.actor_id and film.title like 'ZOOLANDER FICTION';
 6-
-SELECT ADDRESS, country, city, store_id FROM store s, ADDRESS a, country co, city ci WHERE a.address_id = s.address_id AND a.city_id = ci.city_id AND ci.country_id = co.country_id AND s.store_id = 1;
+SELECT address, city, country FROM store , address, city, country WHERE store.address_id = address.address_id AND address.city_id =city.city_id AND city.country_id = country.country_id AND store_id = 1;
 7-
 SELECT DISTINCT f1.title, f2.title, f1.rating FROM film f1 INNER JOIN film f2 ON f1.film_id <> f2.film_id WHERE f1.rating = f2.rating ORDER BY 3, 1, 2;
 8- 
